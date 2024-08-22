1d8a8c73bdf5a66d42b3447e8f1f1ae89114741e
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2024-08-22 17:45:03 -0500

    Usando el componente FAB

--- 02-MyFirstApp/App.tsx
+++ 02-MyFirstApp/App.tsx
@@ -1,12 +1,13 @@
 import {SafeAreaView} from 'react-native';
-import {CounterScreen} from './src/presentation/screens/CounterScreen';
 import {PaperProvider} from 'react-native-paper';
+import {CounterM3Screen} from './src/presentation/screens/CounterM3Screen';
 
 export const App = () => {
   return (
     <PaperProvider>
       <SafeAreaView style={{flex: 1}}>
-        <CounterScreen />
+        {/* <CounterScreen /> */}
+        <CounterM3Screen />
       </SafeAreaView>
     </PaperProvider>
   );
