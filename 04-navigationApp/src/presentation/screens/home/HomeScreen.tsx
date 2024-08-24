3a75b04c17d8850a249fbed9bfc39166b64770eb
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2024-08-24 18:20:23 -0500

    Drawer Navigation

--- 04-navigationApp/src/presentation/screens/home/HomeScreen.tsx
+++ 04-navigationApp/src/presentation/screens/home/HomeScreen.tsx
@@ -1,11 +1,28 @@
-import {View} from 'react-native';
+import {useEffect} from 'react';
+import {Pressable, Text, View} from 'react-native';
 import {globalStyles} from '../../theme/theme';
-import {type NavigationProp, useNavigation} from '@react-navigation/native';
+import {
+  DrawerActions,
+  type NavigationProp,
+  useNavigation,
+} from '@react-navigation/native';
 import {PrimaryButton} from '../../components/shared/PrimaryButton';
 import {type RootStackParams} from '../../routes/StackNavigator';
 
 export const HomeScreen = () => {
   const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <Pressable
+          onPress={() => navigation.dispatch(DrawerActions.toggleDrawer)}>
+          <Text style={{color: 'black'}}>Menu</Text>
+        </Pressable>
+      ),
+    });
+  }, []);
+
   return (
     <View style={globalStyles.container}>
       <PrimaryButton
--- 04-navigationApp/src/presentation/screens/profile/ProfileScreen.tsx
+++ 04-navigationApp/src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,9 +1,19 @@
 import {Text, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {DrawerActions, useNavigation} from '@react-navigation/native';
+import {globalStyles} from '../../theme/theme';
+import {PrimaryButton} from '../../components/shared/PrimaryButton';
 
 export const ProfileScreen = () => {
+  const {top} = useSafeAreaInsets();
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View style={{...globalStyles.container, marginTop: top}}>
+      <Text style={{color: 'black', marginBottom: 12}}>ProfileScreen</Text>
+      <PrimaryButton
+        label="Abrir Menú"
+        onPress={() => navigation.dispatch(DrawerActions.toggleDrawer)}
+      />
     </View>
   );
 };
