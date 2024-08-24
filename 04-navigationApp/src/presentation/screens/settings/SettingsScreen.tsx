40653ea567e9054d8157db4ce9c799d870e6b90b
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2024-08-24 17:12:06 -0500

    Stack - PopToTop

--- 04-navigationApp/src/presentation/screens/settings/SettingsScreen.tsx
+++ 04-navigationApp/src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,9 +1,27 @@
 import {Text, View} from 'react-native';
+import {StackActions, useNavigation} from '@react-navigation/native';
+import {globalStyles} from '../../theme/theme';
+import {PrimaryButton} from '../../components/shared/PrimaryButton';
 
 export const SettingsScreen = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>SettingsScreen</Text>
+    <View style={globalStyles.container}>
+      <Text style={{fontSize: 20, color: 'black', marginBottom: 12}}>
+        Settings Screen
+      </Text>
+      <PrimaryButton
+        label="Go Back"
+        onPress={() => {
+          navigation.goBack();
+        }}
+      />
+      <PrimaryButton
+        label="Go to Home"
+        onPress={() => {
+          navigation.dispatch(StackActions.popToTop());
+        }}
+      />
     </View>
   );
 };
