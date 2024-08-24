99584d04d45c158b7d4f1e2aa7792e85b88d989c
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2024-08-24 16:21:09 -0500

    Navegar entre pantallas

--- 04-navigationApp/src/presentation/screens/home/HomeScreen.tsx
+++ 04-navigationApp/src/presentation/screens/home/HomeScreen.tsx
@@ -1,9 +1,20 @@
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
+import {globalStyles} from '../../theme/theme';
+import {useNavigation} from '@react-navigation/native';
+import {PrimaryButton} from '../../components/shared/PrimaryButton';
 
 export const HomeScreen = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={globalStyles.container}>
+      <PrimaryButton
+        label="Go to Products"
+        onPress={() => navigation.navigate('Products' as never)}
+      />
+      <PrimaryButton
+        label="Go to Settings"
+        onPress={() => navigation.navigate('Settings' as never)}
+      />
     </View>
   );
 };
--- 04-navigationApp/src/presentation/theme/theme.ts
+++ 04-navigationApp/src/presentation/theme/theme.ts
@@ -1,5 +1,33 @@
 import {StyleSheet} from 'react-native';
 
-export const globalColors = {};
+export const globalColors = {
+  primary: '#7037EB',
+  secondary: '#F72585',
+  tertiary: '#3A0CA3',
+  success: '#4CC9F0',
+  warning: '#FCA311',
+  danger: '#E71D36',
+  dark: '#22223b',
+  background: '#FFF',
+};
 
-export const globalStyles = StyleSheet.create({});
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: globalColors.background,
+  },
+  primaryButton: {
+    backgroundColor: globalColors.primary,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    borderRadius: 8,
+    width: '100%',
+    alignItems: 'center',
+    marginBottom: 12,
+  },
+  buttonText: {
+    color: globalColors.background,
+    fontSize: 16,
+  },
+});
