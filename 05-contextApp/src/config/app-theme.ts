83935b88ff3162f2fc4862fc0b521c3b2be8ff77
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2024-08-25 12:36:45 -0500

    Inicializando el proyecto de Context App

---
+++ 05-contextApp/src/config/app-theme.ts
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+    marginTop: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  primaryButton: {
+    backgroundColor: '#DADADA',
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    borderRadius: 4,
+    marginVertical: 10,
+  },
+});
