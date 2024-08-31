4b4147ff750a3cce092bb3bfdc22fb913710d575
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2024-08-31 17:28:42 -0500

    Carrusel con FlatList

--- 06-moviesApp/src/presentation/screens/home/HomeScreen.tsx
+++ 06-moviesApp/src/presentation/screens/home/HomeScreen.tsx
@@ -3,10 +3,11 @@ import {useMovies} from '../../hooks/useMovies';
 import {ScrollView} from 'react-native-gesture-handler';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {PosterCarousel} from '../../components/movies/PosterCarousel';
+import {HorizontalCarousel} from '../../components/movies/HorizontalCarousel';
 
 export const HomeScreen = () => {
   const {top} = useSafeAreaInsets();
-  const {isLoading, nowPlaying} = useMovies();
+  const {isLoading, nowPlaying, popular, topRated, upcoming} = useMovies();
 
   if (isLoading) {
     return (
@@ -19,7 +20,14 @@ export const HomeScreen = () => {
   return (
     <ScrollView>
       <View style={{marginTop: top + 20, paddingBottom: 30}}>
+        {/* Carrusel Principal */}
         <PosterCarousel movies={nowPlaying} />
+        {/* Peliculas Populares */}
+        <HorizontalCarousel movies={popular} title="Populares" />
+        {/* Peliculas Top Rated */}
+        <HorizontalCarousel movies={topRated} title="Mejor puntuadas" />
+        {/* Peliculas proximamente */}
+        <HorizontalCarousel movies={upcoming} title="Próximamente" />
       </View>
     </ScrollView>
   );
