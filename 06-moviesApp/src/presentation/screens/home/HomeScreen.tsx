c4941c3b65e6087da4ef3a1e94cd471e7cce7a99
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2024-08-31 16:55:12 -0500

    Carrusel con ScrollView

--- 06-moviesApp/src/presentation/screens/home/HomeScreen.tsx
+++ 06-moviesApp/src/presentation/screens/home/HomeScreen.tsx
@@ -1,12 +1,26 @@
 import {Text, View} from 'react-native';
 import {useMovies} from '../../hooks/useMovies';
+import {ScrollView} from 'react-native-gesture-handler';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {PosterCarousel} from '../../components/movies/PosterCarousel';
 
 export const HomeScreen = () => {
-  const {} = useMovies();
+  const {top} = useSafeAreaInsets();
+  const {isLoading, nowPlaying} = useMovies();
+
+  if (isLoading) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
 
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={{marginTop: top + 20, paddingBottom: 30}}>
+        <PosterCarousel movies={nowPlaying} />
+      </View>
+    </ScrollView>
   );
 };
