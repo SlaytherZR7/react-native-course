5ca82fdbbd83785e2f33cd355a0bb8966375017d
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2024-08-31 20:49:39 -0500

    Uso de variables de entorno

--- 06-moviesApp/src/config/adapters/movieDB.adapter.ts
+++ 06-moviesApp/src/config/adapters/movieDB.adapter.ts
@@ -1,9 +1,11 @@
+import {THE_MOVIE_DB_KEY} from '@env';
 import {AxiosAdapter} from './http/axios.adapter';
 
 export const movieDBFetcher = new AxiosAdapter({
   baseURL: 'https://api.themoviedb.org/3/movie',
   params: {
-    api_key: '1f66248e3726b43f4435e848a66dd3d5',
+    // api_key: '1f66248e3726b43f4435e848a66dd3d5',
+    api_key: THE_MOVIE_DB_KEY,
     language: 'es',
   },
 });
