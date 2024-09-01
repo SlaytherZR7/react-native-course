c7ca44ce340e30fa17d158f6c4b61d28789b22a5
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2024-09-01 17:30:36 -0500

    Switch Screen

---
+++ 07-componentsApp/src/presentation/screens/switches/SwitchScreen.tsx
@@ -0,0 +1,47 @@
+import {useState} from 'react';
+import {StyleSheet} from 'react-native';
+import {CustomView} from '../../components/ui/CustomView';
+import {Card} from '../../components/ui/Card';
+import {CustomSwitch} from '../../components/ui/CustomSwitch';
+
+export const SwitchScreen = () => {
+  const [state, setState] = useState({
+    isActive: true,
+    isHungry: false,
+    isHappy: true,
+  });
+
+  return (
+    <CustomView>
+      <Card>
+        <CustomSwitch
+          isOn={state.isActive}
+          onChange={value => setState({...state, isActive: value})}
+          text="¿Está Activo?"
+        />
+      </Card>
+      <Card>
+        <CustomSwitch
+          isOn={state.isHungry}
+          onChange={value => setState({...state, isHungry: value})}
+          text="¿Tiene hambre?"
+        />
+      </Card>
+      <Card>
+        <CustomSwitch
+          isOn={state.isHappy}
+          onChange={value => setState({...state, isHappy: value})}
+          text="¿Está feliz?"
+        />
+      </Card>
+    </CustomView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
