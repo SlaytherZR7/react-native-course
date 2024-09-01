a19f6b5219b0502c76819bc98ca4c46f58ac98f4
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2024-09-01 17:07:57 -0500

    Layout

---
+++ 07-componentsApp/src/presentation/components/ui/CustomView.tsx
@@ -0,0 +1,11 @@
+import {StyleProp, View, ViewStyle} from 'react-native';
+import {globalStyles} from '../../../config/theme/theme';
+
+interface Props {
+  style?: StyleProp<ViewStyle>;
+  children?: React.ReactNode;
+}
+
+export const CustomView = ({style, children}: Props) => {
+  return <View style={[globalStyles.mainContainer, style]}>{children}</View>;
+};
