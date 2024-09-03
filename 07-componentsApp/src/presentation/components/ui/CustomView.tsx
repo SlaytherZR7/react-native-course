1ea41d2efbcf0791920517ab91e1133c3078cf43
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2024-09-03 10:27:30 -0500

    update custom view

--- 07-componentsApp/src/presentation/components/ui/CustomView.tsx
+++ 07-componentsApp/src/presentation/components/ui/CustomView.tsx
@@ -4,8 +4,18 @@ import {globalStyles} from '../../../config/theme/theme';
 interface Props {
   style?: StyleProp<ViewStyle>;
   children?: React.ReactNode;
+  margin?: boolean;
 }
 
-export const CustomView = ({style, children}: Props) => {
-  return <View style={[globalStyles.mainContainer, style]}>{children}</View>;
+export const CustomView = ({style, children, margin = false}: Props) => {
+  return (
+    <View
+      style={[
+        globalStyles.mainContainer,
+        margin ? globalStyles.globalMargin : null,
+        style,
+      ]}>
+      {children}
+    </View>
+  );
 };
