47f0a1ba83f4b1d6fbf99a91b843eacbe5a9b7ec
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2024-09-04 09:36:52 -0500

    DarkMode con ReactNativePaper

--- 08-pokedex/src/presentations/PokedexApp.tsx
+++ 08-pokedex/src/presentations/PokedexApp.tsx
@@ -1,14 +1,11 @@
 import 'react-native-gesture-handler';
-import {NavigationContainer} from '@react-navigation/native';
 import {StackNavigator} from './navigator/StackNavigator';
-import {PaperProvider} from 'react-native-paper';
+import {ThemeContextProvider} from './context/ThemeContext';
 
 export const PokedexApp = () => {
   return (
-    <PaperProvider>
-      <NavigationContainer>
-        <StackNavigator />
-      </NavigationContainer>
-    </PaperProvider>
+    <ThemeContextProvider>
+      <StackNavigator />
+    </ThemeContextProvider>
   );
 };
