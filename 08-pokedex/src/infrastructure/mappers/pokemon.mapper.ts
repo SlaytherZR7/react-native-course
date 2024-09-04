32a7494519278275d98cfeb849f20c5c68a04200
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2024-09-04 18:42:27 -0500

    Add aditional information on Pokemon

--- 08-pokedex/src/infrastructure/mappers/pokemon.mapper.ts
+++ 08-pokedex/src/infrastructure/mappers/pokemon.mapper.ts
@@ -1,6 +1,6 @@
 import {getColorFromImage} from '../../config/helpers/get-color';
-import {Pokemon} from '../../domain/entities/pokemon';
-import {PokeAPIPokemon} from '../interfaces/pokeapi.interfaces';
+import type {Pokemon} from '../../domain/entities/pokemon';
+import type {PokeAPIPokemon} from '../interfaces/pokeapi.interfaces';
 
 export class PokemonMapper {
   static async mapPokemonFromApiToEntity(
@@ -8,15 +8,29 @@ export class PokemonMapper {
   ): Promise<Pokemon> {
     const sprites = PokemonMapper.getSprites(data);
     const avatar = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`;
+
     const color = await getColorFromImage(avatar);
 
     return {
       id: data.id,
-      avatar,
       name: data.name,
-      sprites,
+      avatar,
+      sprites: sprites,
       types: data.types.map(type => type.type.name),
       color,
+
+      games: data.game_indices.map(game => game.version.name),
+      stats: data.stats.map(stat => ({
+        name: stat.stat.name,
+        value: stat.base_stat,
+      })),
+      abilities: data.abilities.map(ability => ability.ability.name),
+      moves: data.moves
+        .map(move => ({
+          name: move.move.name,
+          level: move.version_group_details[0].level_learned_at,
+        }))
+        .sort((a, b) => a.level - b.level),
     };
   }
 
--- 08-pokedex/src/presentations/screens/pokemon/PokemonScreen.tsx
+++ 08-pokedex/src/presentations/screens/pokemon/PokemonScreen.tsx
@@ -13,7 +13,7 @@ import {ThemeContext} from '../../context/ThemeContext';
 
 interface Props extends StackScreenProps<RootStackParams, 'PokemonScreen'> {}
 
-export const PokemonScreen = ({navigation, route}: Props) => {
+export const PokemonScreen = ({route}: Props) => {
   const {isDark} = useContext(ThemeContext);
   const {top} = useSafeAreaInsets();
   const {pokemonId} = route.params;
@@ -83,6 +83,66 @@ export const PokemonScreen = ({navigation, route}: Props) => {
             uri={item}
             style={{width: 100, height: 100, marginHorizontal: 5}}
           />
+        )}
+      />
+
+      {/* abilities */}
+      <Text style={styles.subTitle}>Abilities</Text>
+      <FlatList
+        data={pokemon.abilities}
+        horizontal
+        keyExtractor={item => item}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({item}) => (
+          <Chip selectedColor="white">{Formatter.capitalize(item)}</Chip>
+        )}
+      />
+
+      {/* Stats */}
+      <Text style={styles.subTitle}>Stats</Text>
+
+      <FlatList
+        data={pokemon.stats}
+        keyExtractor={item => item.name}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({item}) => (
+          <View style={styles.statsContainer}>
+            <Text style={{flex: 1, color: 'white'}}>
+              {Formatter.capitalize(item.name)}
+            </Text>
+            <Text style={{color: 'white'}}>{item.value}</Text>
+          </View>
+        )}
+      />
+
+      {/* Moves */}
+      <Text style={styles.subTitle}>Moves</Text>
+      <FlatList
+        data={pokemon.moves}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        centerContent
+        renderItem={({item}) => (
+          <View style={styles.statsContainer}>
+            <Text style={{flex: 1, color: 'white'}}>
+              {Formatter.capitalize(item.name)}
+            </Text>
+            <Text style={{color: 'white'}}>lvl {item.level}</Text>
+          </View>
+        )}
+      />
+
+      {/* Games */}
+      <Text style={styles.subTitle}>Games</Text>
+      <FlatList
+        data={pokemon.games}
+        horizontal
+        keyExtractor={item => item}
+        showsHorizontalScrollIndicator={false}
+        centerContent
+        renderItem={({item}) => (
+          <Chip selectedColor="white">{Formatter.capitalize(item)}</Chip>
         )}
       />
 
