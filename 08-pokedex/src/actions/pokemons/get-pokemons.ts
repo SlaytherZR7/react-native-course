a56e3ff187ea19801247a1bc57d4c91738dc6d3c
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2024-09-04 10:24:15 -0500

    Interfaces de PokeAPI

--- 08-pokedex/src/actions/pokemons/get-pokemons.ts
+++ 08-pokedex/src/actions/pokemons/get-pokemons.ts
@@ -1,11 +1,24 @@
 import {pokeApi} from '../../config/api/pokeApi';
 import {Pokemon} from '../../domain/entities/pokemon';
+import {
+  PokeAPIPaginatedResponse,
+  PokeAPIPokemon,
+} from '../../infrastructure/interfaces/pokeapi.interfaces';
 
-export const getPokemons = async (): Promise<Pokemon[]> => {
+export const getPokemons = async (
+  page: number,
+  limit: number = 20,
+): Promise<Pokemon[]> => {
   try {
-    const URL = '/pokemon';
-    const {data} = await pokeApi.get(URL);
-    console.log(data);
+    const URL = `/pokemon?offset=${page * 10}&limit=${limit}`;
+    const {data} = await pokeApi.get<PokeAPIPaginatedResponse>(URL);
+
+    const pokemonPromises = data.results.map(info => {
+      return pokeApi.get<PokeAPIPokemon>(info.url);
+    });
+
+    const pokeApiPokemons = await Promise.all(pokemonPromises);
+
     return [];
   } catch (error) {
     throw new Error('Error getting pokemons');
