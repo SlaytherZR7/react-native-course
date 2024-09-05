afb2db7e9dd3600e991ae1aa4f4b79b06a21b498
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2024-09-05 18:35:32 -0500

    Request Permission

---
+++ 09-MapsApp/src/actions/permissions/location.ts
@@ -0,0 +1,35 @@
+import {
+  openSettings,
+  PERMISSIONS,
+  request,
+  PermissionStatus as RNPermissionStatus,
+} from 'react-native-permissions';
+import type {PermissionStatus} from '../../infrastructure/interfaces/permissions';
+import {Platform} from 'react-native';
+
+export const requestLocationPermission =
+  async (): Promise<PermissionStatus> => {
+    let status: RNPermissionStatus = 'unavailable';
+
+    if (Platform.OS === 'ios') {
+      status = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+    } else if (Platform.OS === 'android') {
+      status = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+    } else {
+      throw new Error('Unsupported platform');
+    }
+
+    if (status === 'blocked') {
+      await openSettings();
+    }
+
+    const permissionMapper: Record<RNPermissionStatus, PermissionStatus> = {
+      granted: 'granted',
+      denied: 'denied',
+      blocked: 'blocked',
+      limited: 'limited',
+      unavailable: 'unavailable',
+    };
+
+    return permissionMapper[status] ?? 'unavilable';
+  };
