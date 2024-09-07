1d83d2b6efb700e330f2da769e0d794df6f958ed
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2024-09-07 16:28:54 -0500

    Acciones de localización

---
+++ 09-MapsApp/src/actions/location/location.ts
@@ -0,0 +1,20 @@
+import Geolocation from '@react-native-community/geolocation';
+import {Location} from '../../infrastructure/interfaces/location';
+
+export const getCurrentLocation = async (): Promise<Location> => {
+  return new Promise((resolve, reject) => {
+    Geolocation.getCurrentPosition(
+      info => {
+        resolve({
+          latitude: info.coords.latitude,
+          longitude: info.coords.longitude,
+        });
+      },
+      error => {
+        console.error('Cannot get current location', error);
+        reject(error);
+      },
+      {enableHighAccuracy: true},
+    );
+  });
+};
